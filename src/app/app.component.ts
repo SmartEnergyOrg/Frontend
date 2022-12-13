4e7c303cc001f89ef36ef132a38ce42dcd9af8a5
xin <xin20wang@outlook.com>
2022-12-13 16:25:01 +0100

    Chartjs version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {Chart} from 'chart.js/auto';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'StatusMonitor';
+  public chart: any;
+
+  ngOnInit() {
+    this.createChart();
+  }
+
+  createChart(){
+
+    this.chart = new Chart("myChart", {
+      type: 'doughnut', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+          '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ],
+        datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+              '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+              '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+
+    });
+  }
 }
