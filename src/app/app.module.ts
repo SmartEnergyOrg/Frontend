b49a224095dac840f2bddc1a707a3be95f5924b1
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2022-12-15 10:05:32 +0100

    added shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BarChartComponent } from './shared/widgets/bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule,
