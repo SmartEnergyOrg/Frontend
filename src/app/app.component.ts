0b36eb57a4a46bf591af784ff130faa9199d74c0
mj.holster <mj.holster@student.avans.nl>
2022-12-16 12:35:45 +0100

    Added NavBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {Chart} from 'chart.js/auto';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'StatusMonitor';
   public chart: any;
 
@@ -13,33 +14,38 @@ export class AppComponent implements OnInit{
     this.createChart();
   }
 
-  createChart(){
-
-    this.chart = new Chart("myChart", {
+  createChart() {
+    this.chart = new Chart('myChart', {
       type: 'doughnut', //this denotes tha type of chart
 
-      data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-          '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ],
+      data: {
+        // values on X-Axis
+        labels: [
+          '2022-05-10',
+          '2022-05-11',
+          '2022-05-12',
+          '2022-05-13',
+          '2022-05-14',
+          '2022-05-15',
+          '2022-05-16',
+          '2022-05-17',
+        ],
         datasets: [
           {
-            label: "Sales",
-            data: ['467','576', '572', '79', '92',
-              '574', '573', '576'],
-            backgroundColor: 'blue'
+            label: 'Sales',
+            data: ['467', '576', '572', '79', '92', '574', '573', '576'],
+            backgroundColor: 'blue',
           },
           {
-            label: "Profit",
-            data: ['542', '542', '536', '327', '17',
-              '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }
-        ]
+            label: 'Profit',
+            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
+            backgroundColor: 'limegreen',
+          },
+        ],
       },
       options: {
-        aspectRatio:2.5
-      }
-
+        aspectRatio: 2.5,
+      },
     });
   }
 }
