250c46e66da58415225e4da730adf0fe24154a2a
Joost <ja.vandam3@student.avans.nl>
2022-12-21 11:00:11 +0100

    routing fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardRoutingModule } from './pages/dashboard/dashboard-routing.module';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { SettingsComponent } from './shared/settings/settings.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    component: AppComponent,
-  },
   {
     path: 'settings',
     pathMatch: 'full',
