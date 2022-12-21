f29dc4697a0854bf1778fcfc991ee1029ecf172c
Joost <ja.vandam3@student.avans.nl>
2022-12-21 11:28:11 +0100

    form initial

--- src/app/pages/dashboard/form/form.component.ts
+++ src/app/pages/dashboard/form/form.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  componentExists = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
