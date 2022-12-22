70548dde331a46d54a2f213b7c63ec91adaa448d
Joost <ja.vandam3@student.avans.nl>
2022-12-22 11:30:35 +0100

    widgetService addWidget implemented

--- src/app/shared/widget/widget.service.ts
+++ src/app/shared/widget/widget.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, throwError } from 'rxjs';
 import { Widget } from 'src/app/models/widget.model';
 import { environment } from 'src/environments/environment';
 
@@ -8,36 +8,51 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class WidgetService {
-  private readonly apiUrl = environment.api_url;
+  private readonly SERVER_API_URL = environment.SERVER_API_URL;
 
   constructor(private readonly httpClient: HttpClient) {}
 
   getAll() {
     console.log('Get all widgets');
-    return this.httpClient.get<any>(this.apiUrl + '/api/widgetsConfig');
+    return this.httpClient.get<any>(this.SERVER_API_URL + '/api/widgetsConfig');
   }
 
   // waarom is het nu .get<any> we weten toch dat het een widget is?
   getWidgetById(id: number) {
     console.log('Get all widgets');
-    return this.httpClient.get<any>(`${this.apiUrl}/api/widgetsConfig/${id}`);
+    return this.httpClient.get<any>(
+      `${this.SERVER_API_URL}/api/widgetsConfig/${id}`
+    );
   }
 
   // de parameter moet widget id zijn
   getDataOfWidget(widget: Widget) {
     console.log(`Get data of widget with id ${widget.id}`);
     return this.httpClient.get<any>(
-      `${this.apiUrl}/api/widgetsConfig/poll/${widget.id}`
+      `${this.SERVER_API_URL}/api/widgetsConfig/poll/${widget.id}`
     );
   }
 
   addWidget(widget: Widget): Observable<string> {
-    //add implementatie
-    return of('test');
+    const endpoint = `${environment.SERVER_API_URL}/api/widgetsConfig`;
+
+    return this.httpClient
+      .post<Widget>(endpoint, widget)
+      .pipe(catchError(this.handleError));
   }
 
   updateWidget(widget: Widget): Observable<string> {
     //update implementatie
     return of('test');
+  }
+
+  public handleError(error: HttpErrorResponse): Observable<any> {
+    // console.log(error);
+
+    const errorResponse = {
+      type: 'error',
+      message: error.error.message || error.message,
+    };
+    return throwError(errorResponse);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_url: "http://localhost:3333"
+  SERVER_API_URL: 'http://localhost:3333',
 };
 
 /*
