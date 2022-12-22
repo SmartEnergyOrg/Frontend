6a8e01ad90ed3c0a9c553dc399108cacc3d19e9e
Joost <ja.vandam3@student.avans.nl>
2022-12-22 11:22:52 +0100

    fixed minor bug for widget interface

--- src/app/pages/dashboard/index/index.component.ts
+++ src/app/pages/dashboard/index/index.component.ts
@@ -2,38 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Widget } from 'src/app/models/widget.model';
 import { WidgetService } from 'src/app/shared/widget/widget.service';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
   widgetSubscription: Subscription | undefined;
   widgets: Widget[] = [];
 
-  constructor(
-    private readonly widgetService: WidgetService
-  ) { }
+  constructor(private readonly widgetService: WidgetService) {}
 
   ngOnInit(): void {
     this.widgetSubscription = this.widgetService.getAll().subscribe({
-      next: res => {
-        res = res.result
-        res.forEach((element: any)=> {
-          this.widgets.push(
-            new Widget(
-              element.WidgetId,
-              element.Title,
-              element.DefaultRange,
-              element.Color_Graph
-            )
-          )
+      next: (res) => {
+        res = res.result;
+
+        let widget: Widget = {
+          id: '',
+          title: '',
+          defaultRange: '',
+          color: '',
+          type: 'bar',
+        };
+
+        res.forEach((element: any) => {
+          (widget.id = element.WidgetId),
+            (widget.title = element.Title),
+            (widget.defaultRange = element.DefaultRange),
+            (widget.color = element.Color_Graph);
+          this.widgets.push(widget);
         });
       },
-      error: err => {
+      error: (err) => {
         // TODO implement error handling
-      }
-    })
+      },
+    });
   }
 }
