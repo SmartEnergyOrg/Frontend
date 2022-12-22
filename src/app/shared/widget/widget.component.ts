e944611b6f951d41da3d824f400d6d6fafe80633
mj.holster <mj.holster@student.avans.nl>
2022-12-22 14:27:32 +0100

    Widgets are now draggable

--- src/app/shared/widget/widget.component.ts
+++ src/app/shared/widget/widget.component.ts
@@ -7,33 +7,30 @@ import { v4 as uuid } from 'uuid';
 @Component({
   selector: 'app-widget',
   templateUrl: './widget.component.html',
-  styleUrls: ['./widget.component.css']
+  styleUrls: ['./widget.component.css'],
 })
 export class WidgetComponent implements OnInit {
   // TODO implement better random id
-  chartId: string = uuid ()
+  chartId: string = uuid();
 
   @Input()
   widget: Widget | undefined;
 
-  constructor(
-    private readonly widgetService: WidgetService
-  ) { }
+  constructor(private readonly widgetService: WidgetService) {}
 
   ngOnInit(): void {
     if (this.widget != undefined) {
       this.widgetService.getDataOfWidget(this.widget).subscribe({
-        next: res => {
+        next: (res) => {
           this.createChart(res);
         },
-        error: err => {
+        error: (err) => {
           // TODO implement error handling
-        }
-      })
+        },
+      });
     } else {
       // TODO implement error handling
     }
-
   }
 
   createChart(data: []) {
@@ -42,21 +39,24 @@ export class WidgetComponent implements OnInit {
         type: this.widget.type,
 
         data: {
-          datasets: [{
-            label: "Solar",
-            data: data.map((row: any) => <any>{
-              x: row._time,
-              y: row._value
-            })
-          }]
+          datasets: [
+            {
+              label: 'Solar',
+              data: data.map(
+                (row: any) =>
+                  <any>{
+                    x: row._time,
+                    y: row._value,
+                  }
+              ),
+            },
+          ],
         },
         options: {
           aspectRatio: 2,
           maintainAspectRatio: false,
-        }
+        },
       });
     }
   }
-
-
 }
