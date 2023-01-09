230fe2ee9c3a7267941968e00d2d9ce74549ae75
xin <xin20wang@outlook.com>
2023-01-09 12:52:47 +0100

    Imported socket.io package and created appropiate methods to use socket.io

--- src/app/shared/widget/widget.service.ts
+++ src/app/shared/widget/widget.service.ts
@@ -4,15 +4,19 @@ import {
   HttpHeaders,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, map, Observable, of, throwError } from 'rxjs';
+import {BehaviorSubject, catchError, map, Observable, of, throwError} from 'rxjs';
 import { Widget } from 'src/app/models/widget.model';
 import { environment } from 'src/environments/environment';
+import { io } from "socket.io-client";
 
 @Injectable({
   providedIn: 'root',
 })
 export class WidgetService {
+
   private readonly SERVER_API_URL = environment.SERVER_API_URL;
+  private readonly socket = io("wss://localhost:9400");
+
 
   constructor(private readonly httpClient: HttpClient) { }
 
@@ -86,4 +90,29 @@ export class WidgetService {
   //     widget
   //   );
   // }
+
+  //The connectToSocket handles the connection to the socket event.
+  //It receives a widget as parameter
+  //It will return a observable
+  //As parameter, will the method receive a object with the widget configuration
+  //The receiveSocketPayload method will handle the receive of the payload.
+  // TODO Widget object is subject to change.
+  connectToSocket(widget: Widget){
+    this.socket.emit('getGraph', (widget));
+  }
+
+  //The receiveSocketPayload method handles the payload of the socket connection
+  //When it receives new data from the server
+  //This method will create widgetSubject, every time it is called upon.
+  //It will then return observable of this behavioursubject.
+  //Everytime a new event has been called it will update the behaviour subject
+  // TODO Any will need to be changed to appropiate object.
+  receiveSocketPayload(widget: Widget):Observable<any>{
+    const WidgetSubject = new BehaviorSubject<any>(undefined);
+    this.socket.on('getGraph', (payload)=>{
+      console.log(this.socket.connected);
+      WidgetSubject.next(payload);
+    })
+    return WidgetSubject.asObservable();
+  }
 }
