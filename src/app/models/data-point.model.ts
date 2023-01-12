442b7ecef9313885ca3abaa76963573758a1f8e8
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2023-01-12 13:14:03 +0100

    added unit to datapoints in model and chart

--- src/app/models/data-point.model.ts
+++ src/app/models/data-point.model.ts
@@ -2,11 +2,13 @@ import { IDataPoint } from '../interfaces/data-point.interface';
 
 export class DataPoint implements IDataPoint {
   measurement: string; // name:unit
+  unit: string;
   value: number;
   time: Date;
 
-  constructor(measurement: string, value: number, time: Date) {
+  constructor(measurement: string, unit: string, value: number, time: Date) {
     this.measurement = measurement;
+    this.unit = unit;
     this.value = value;
     this.time = time;
   }
