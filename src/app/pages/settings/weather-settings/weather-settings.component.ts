73bc63ab9bd58b6e68cea3577009e2aba818af3d
Xin Wang <xx.wang@student.avans.nl>
2023-01-14 00:20:48 +0100

    Weather settings form is setup. Weather can now be configured

---
+++ src/app/pages/settings/weather-settings/weather-settings.component.ts
@@ -0,0 +1,78 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { WeatherConfig } from 'src/app/models/weather.model';
+import { WeatherService } from 'src/app/shared/nav/weather.service';
+import {fromEvent, Observable, of, OperatorFunction} from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, map, tap, switchMap } from 'rxjs/operators';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-weather-settings',
+  templateUrl: './weather-settings.component.html',
+  styleUrls: ['./weather-settings.component.css']
+})
+export class WeatherSettingsComponent implements OnInit {
+
+  error: string = "";
+
+  weatherSettings!: WeatherConfig;
+
+  searchTerm: string;
+
+  cityList: WeatherConfig[] = [];
+  foundCity:boolean = false;
+
+
+  constructor(private weatherService: WeatherService, private router: Router) {
+    this.searchTerm = "";
+    this.weatherSettings = {
+      name: "",
+      country: "",
+      lat: 0,
+      lon: 0
+    }
+  }
+
+  choseCity(city: WeatherConfig){
+    this.weatherSettings = city;
+    this.cityList = [];
+  }
+
+  ngOnInit(): void {
+    this.weatherService.getConfig().subscribe((v)=>{
+      if (v){
+        this.weatherSettings = v;
+        this.foundCity = true;
+      } else{
+        this.foundCity = false;
+      }
+    })
+  }
+
+  //Used to show all cities that matches the searchTerm.
+  searchPlace(){
+    this.weatherService
+    .connectWeatherPlace(this.searchTerm)
+    .subscribe(
+      (place)=>{
+      console.log(place);
+      if(place.length > 0){
+        this.error = "";
+        this.foundCity = true;
+        this.cityList = place;
+      }else{
+        this.error = "Geen stad gevonden. Voer een bestaande woonplaats in";
+      }
+    })
+  }
+
+  onSubmit(){
+    console.log("Push naar db");
+    // this.weatherService.create(this.weatherSettings).subscribe((v)=>{
+    //   console.log(this.weatherSettings);
+    //   this.weatherService.assignToConfig(this.weatherSettings);
+    // })
+    console.log(this.weatherSettings);
+    this.weatherService.assignToConfig(this.weatherSettings);
+    this.router.navigate(['..']);
+  }
+}
