5bb956ae847f77fb3de5e3c676ce608991e55bb3
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2023-01-17 16:27:06 +0100

    updated routing endpoints

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,26 +6,39 @@ import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.co
 import { SettingsComponent } from './pages/settings/settings.component';
 import { DeleteFormComponent } from './pages/settings/delete-form/delete-form.component';
 import { WeatherSettingsComponent } from './pages/settings/weather-settings/weather-settings.component';
+import { IndexComponent as DashboardIndexComponent } from './pages/dashboard/index/index.component';
+import { FormComponent as DashboardFormComponent } from './pages/dashboard/form/form.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'dashboard'
+    component: DashboardIndexComponent
+  },
+  {
+    path: 'settings/edit/:id',
+    component: DashboardFormComponent,
+  },
+  {
+    path: 'settings/create',
+    component: DashboardFormComponent,
   },
   {
     path: 'settings',
     component: SettingsComponent,
   },
   {
-    path: 'settings/delete-form/:id',
+    path: 'settings/delete/:id',
     component: DeleteFormComponent
   },
   {
-    path: 'settings/weather-settings',
+    path: 'settings/weather',
     component: WeatherSettingsComponent
-  }
+  },
 
+  //Wild Card Route for 404 request
+  { path: '**', pathMatch: 'full',
+      component: PageNotFoundComponent },
 ];
 
 @NgModule({
