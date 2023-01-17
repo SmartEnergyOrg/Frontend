dc57ad41d4612d6466dd04b34258515ff7597b84
mj.holster <mj.holster@student.avans.nl>
2023-01-17 02:09:52 +0100

    Added amount  of queries equals amount of y-axes and show Data not Available when Query does not show data

--- src/app/shared/widget/widget.component.ts
+++ src/app/shared/widget/widget.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { Chart, Point } from 'chart.js/auto';
 import { Widget } from 'src/app/models/widget.model';
 import { WidgetService } from './widget.service';
 import { v4 as uuid } from 'uuid';
 import { formatDate } from '@angular/common';
-import { interval, Observable, skipWhile, Subscription, take } from 'rxjs';
+import {
+  interval,
+  Observable,
+  skipWhile,
+  startWith,
+  Subscription,
+  take,
+} from 'rxjs';
 import { Graph } from 'src/app/models/graph.model';
 import { DataPoint } from 'src/app/models/data-point.model';
 import 'chartjs-adapter-moment';
@@ -25,12 +32,17 @@ export class WidgetComponent implements OnInit {
 
   chart: Chart | undefined;
 
+  dataReceived: boolean = false;
+
   // Checks if  widget is a singlestat
   // isSingleStat is true when a singlestat graph is present
   // If there is no single-stat graph, it will always return false as its default value.
   isSingleStat: boolean = false;
 
-  constructor(private readonly widgetService: WidgetService) {}
+  constructor(
+    private readonly widgetService: WidgetService,
+    private elementRef: ElementRef
+  ) {}
 
   private assertInputsProvided(): void {
     if (!this.widget) {
@@ -54,13 +66,27 @@ export class WidgetComponent implements OnInit {
     const datasets: any = [];
     const chartContainer = document.getElementById('chart-container');
 
+    if (!chartContainer) {
+      throw new Error('chart-container element not found in template');
+    }
+
     this.widget.graphs.forEach((graph, idx) => {
       //Subscribe to graph.data
       this.subscriptions.push(
         graph.data
-          .pipe(skipWhile((value) => !value)) // skip null values
+          .pipe(
+            startWith([]),
+            skipWhile((value) => !value)
+          ) // skip null values
           .subscribe((value) => {
+            if (value.length <= 0 && !this.chart) {
+              if (!this.dataReceived) {
+                chartContainer.innerHTML = 'No Data Available';
+              }
+              return;
+            }
             if (value.length > 0) {
+              this.dataReceived = true;
               const [{ measurement, unit }] = value!;
 
               const data = value!.map(({ time, value }) => ({
@@ -85,6 +111,39 @@ export class WidgetComponent implements OnInit {
                 dataset.yAxisID = index == 0 ? 'y' : 'y' + index;
               });
 
+              //Get scales to know the amount of Y axes ChartJS needs to use.
+              function getScales(numDatasets: number) {
+                const scales: any = {
+                  x: {
+                    type: 'timeseries',
+                  },
+                  y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    ticks: {
+                      color: datasets[0].borderColor,
+                    },
+                  },
+                };
+
+                for (let i = 1; i < numDatasets; i++) {
+                  scales[`y${i}`] = {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    ticks: {
+                      color: datasets[i].borderColor,
+                    },
+                    grid: {
+                      drawOnChartArea: false,
+                    },
+                  };
+                }
+
+                return scales;
+              }
+
               if (datasets.length === this.widget.graphs.length) {
                 if (this.chart) this.chart.destroy();
                 this.chart = new Chart(this.chartId, {
@@ -107,34 +166,9 @@ export class WidgetComponent implements OnInit {
                     animation: {
                       duration: 0,
                     },
-                    scales: {
-                      x: {
-                        type: 'timeseries',
-                      },
-                      y: {
-                        type: 'linear',
-                        display: true,
-                        position: 'left',
-                        ticks: {
-                          color: datasets[0].borderColor,
-                        },
-                      },
-                      y1: {
-                        type: 'linear',
-                        display: true,
-                        position: 'right',
-                        ticks: {
-                          color: datasets[1].borderColor,
-                        },
-                        grid: {
-                          drawOnChartArea: false,
-                        },
-                      },
-                    },
+                    scales: getScales(datasets.length),
                   },
                 });
-              } else {
-                chartContainer!.innerHTML = 'No Data Available';
               }
             }
           })
