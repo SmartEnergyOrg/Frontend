4816fc0edfea340585312df2608ab9b077050183
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2023-01-17 13:17:13 +0100

    updated dir name of dashboard create/edit form

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormComponent } from './form/form.component';
+import { FormComponent } from './create-edit-form/form.component';
 import { IndexComponent } from './index/index.component';
 import { ViewComponent } from './view/view.component';
 
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { FormComponent } from './form/form.component';
+import { FormComponent } from './create-edit-form/form.component';
 import { IndexComponent } from './index/index.component';
 import { ViewComponent } from './view/view.component';
 import { WidgetComponent } from 'src/app/shared/widget/widget.component';
