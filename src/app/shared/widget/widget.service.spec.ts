d5cf9f05765b99228851876e768eaeb689123308
mj.holster <mj.holster@student.avans.nl>
2023-01-18 01:12:24 +0100

    Attempt to fix tests #3

--- src/app/shared/widget/widget.service.spec.ts
+++ src/app/shared/widget/widget.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { WidgetService } from './widget.service';
 
 describe('WidgetService', () => {
   let service: WidgetService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(WidgetService);
   });
 
